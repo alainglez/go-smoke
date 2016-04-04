11edfcf9811b9852ef9c2e6d18875f665f1dba0a
alainglez <alain.gonzalez@gmail.com>
2016-04-04 00:13:04 -0400

    Update router.go

--- routers/router.go
+++ routers/router.go
@@ -8,9 +8,11 @@ func InitRoutes() *mux.Router {
 	router := mux.NewRouter().StrictSlash(false)
 	// Routes for the User entity
 	router = SetUserRoutes(router)
-	// Routes for the Task entity
-	router = SetTaskRoutes(router)
-	// Routes for the TaskNote entity
-	router = SetNoteRoutes(router)
+	// Routes for the Site entity
+	router = SetSiteRoutes(router)
+	// Routes for the SmokeTest entity
+	router = SetSmokeTestRoutes(router)
+	// Routes for the SiteUrl entity
+	router = SetUrlRoutes(router)
 	return router
 }
