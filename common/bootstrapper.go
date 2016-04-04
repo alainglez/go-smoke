9c021f8c482a8d77b5c8005c221dd3c6fe7798af
alainglez <alain.gonzalez@gmail.com>
2016-04-03 22:48:28 -0400

    Update bootstrapper.go

--- common/bootstrapper.go
+++ common/bootstrapper.go
@@ -1,1 +1,12 @@
-import
+package common
+
+func StartUp() {
+	// Initialize AppConfig variable
+	initConfig()
+	// Initialize private/public keys for JWT authentication
+	initKeys()
+	// Start a MongoDB session
+	createDbSession()
+	// Add indexes into MongoDB
+	addIndexes()
+}
