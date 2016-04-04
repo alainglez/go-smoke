7307d50f569e98cbd664ff49224f83982eae13c4
alainglez <alain.gonzalez@gmail.com>
2016-04-04 15:55:40 -0400

    Update smokeRepository.go

--- data/smokeRepository.go
+++ data/smokeRepository.go
@@ -6,57 +6,57 @@ import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
-	"github.com/shijuvar/go-web/taskmanager/models"
+	"github.com/alainglez/go-smoke/models"
 )
 
-type TaskRepository struct {
+type SmokeTestRepository struct {
 	C *mgo.Collection
 }
 
-func (r *TaskRepository) Create(task *models.Task) error {
+func (r *SmokeTestRepository) Create(smoketest *models.SmokeTest) error {
 	obj_id := bson.NewObjectId()
-	task.Id = obj_id
-	task.CreatedOn = time.Now()
-	task.Status = "Created"
-	err := r.C.Insert(&task)
+	smoketest.Id = obj_id
+	smoketest.CreatedOn = time.Now()
+	smoketest.Status = "Created"
+	err := r.C.Insert(&smoketest)
 	return err
 }
 
-func (r *TaskRepository) Update(task *models.Task) error {
+func (r *SmokeTestRepository) Update(smoketest *models.SmokeTest) error {
 	// partial update on MogoDB
-	err := r.C.Update(bson.M{"_id": task.Id},
+	err := r.C.Update(bson.M{"_id": smoketest.Id},
 		bson.M{"$set": bson.M{
-			"name":        task.Name,
-			"description": task.Description,
-			"due":         task.Due,
-			"status":      task.Status,
-			"tags":        task.Tags,
+			"name":        smoketest.Name,
+			"description": smoketest.Description,
+			"due":         smoketest.Due,
+			"status":      smoketest.Status,
+			"tags":        smoketest.Tags,
 		}})
 	return err
 }
-func (r *TaskRepository) Delete(id string) error {
+func (r *SmokeTestRepository) Delete(id string) error {
 	err := r.C.Remove(bson.M{"_id": bson.ObjectIdHex(id)})
 	return err
 }
-func (r *TaskRepository) GetAll() []models.Task {
-	var tasks []models.Task
+func (r *SmokeTestRepository) GetAll() []models.SmokeTest {
+	var smoketests []models.SmokeTest
 	iter := r.C.Find(nil).Iter()
-	result := models.Task{}
+	result := models.SmokeTest{}
 	for iter.Next(&result) {
-		tasks = append(tasks, result)
+		smoketests = append(smoketests, result)
 	}
-	return tasks
+	return smoketests
 }
-func (r *TaskRepository) GetById(id string) (task models.Task, err error) {
-	err = r.C.FindId(bson.ObjectIdHex(id)).One(&task)
+func (r *SmokeTestRepository) GetById(id string) (smoketest models.SmokeTest, err error) {
+	err = r.C.FindId(bson.ObjectIdHex(id)).One(&smoketest)
 	return
 }
-func (r *TaskRepository) GetByUser(user string) []models.Task {
-	var tasks []models.Task
+func (r *SmokeTestRepository) GetByUser(user string) []models.SmokeTest {
+	var smoketests []models.SmokeTest
 	iter := r.C.Find(bson.M{"createdby": user}).Iter()
-	result := models.Task{}
+	result := models.SmokeTest{}
 	for iter.Next(&result) {
-		tasks = append(tasks, result)
+		smoketests = append(smoketests, result)
 	}
-	return tasks
+	return smoketests
 }
