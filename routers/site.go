c1fed8a2b8b74dfc41cfa5864c2214d01e7b702c
alainglez <alain.gonzalez@gmail.com>
2016-04-04 00:07:28 -0400

    Update site.go

--- routers/site.go
+++ routers/site.go
@@ -3,21 +3,21 @@ package routers
 import (
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
-	"github.com/shijuvar/go-web/taskmanager/common"
-	"github.com/shijuvar/go-web/taskmanager/controllers"
+	"github.com/alainglez/go-smoke/common"
+	"github.com/alainglez/go-smoke/controllers"
 )
 
-func SetTaskRoutes(router *mux.Router) *mux.Router {
-	taskRouter := mux.NewRouter()
-	taskRouter.HandleFunc("/tasks", controllers.CreateTask).Methods("POST")
-	taskRouter.HandleFunc("/tasks/{id}", controllers.UpdateTask).Methods("PUT")
-	taskRouter.HandleFunc("/tasks", controllers.GetTasks).Methods("GET")
-	taskRouter.HandleFunc("/tasks/{id}", controllers.GetTaskById).Methods("GET")
-	taskRouter.HandleFunc("/tasks/users/{id}", controllers.GetTasksByUser).Methods("GET")
-	taskRouter.HandleFunc("/tasks/{id}", controllers.DeleteTask).Methods("DELETE")
-	router.PathPrefix("/tasks").Handler(negroni.New(
+func SetSiteRoutes(router *mux.Router) *mux.Router {
+	siteRouter := mux.NewRouter()
+	siteRouter.HandleFunc("/sites", controllers.CreateSite).Methods("POST")
+	siteRouter.HandleFunc("/sites/{id}", controllers.UpdateSite).Methods("PUT")
+	siteRouter.HandleFunc("/sites", controllers.GetSites).Methods("GET")
+	siteRouter.HandleFunc("/sites/{id}", controllers.GetSiteById).Methods("GET")
+	siteRouter.HandleFunc("/sites/users/{id}", controllers.GetSitesByUser).Methods("GET")
+	siteRouter.HandleFunc("/sites/{id}", controllers.DeleteSite).Methods("DELETE")
+	router.PathPrefix("/sites").Handler(negroni.New(
 		negroni.HandlerFunc(common.Authorize),
-		negroni.Wrap(taskRouter),
+		negroni.Wrap(siteRouter),
 	))
 	return router
 }
