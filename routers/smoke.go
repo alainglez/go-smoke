d96a548cbe569bb362330f9c1f6e084283d3d929
alainglez <alain.gonzalez@gmail.com>
2016-04-06 22:54:13 -0400

    Update smoke.go

--- routers/smoke.go
+++ routers/smoke.go
@@ -9,10 +9,13 @@ import (
 
 func SetSmokeTestRoutes(router *mux.Router) *mux.Router {
 	smoketestRouter := mux.NewRouter()
-	smoketestRouter.HandleFunc("/smoketests", controllers.CreateSmokeTest).Methods("POST")
+	smoketestRouter.HandleFunc("/smoketests", controllers.RunSmokeTest).Methods("POST")
+	//PUT not needed as RunSmokeTest updates the PassFail and URLResults and returns them as JSON
+	//smoketest.HandleFunc("/smoketests/{id}", controllers.UpdateSmoketest).Methods("PUT")
 	smoketestRouter.HandleFunc("/smoketests", controllers.GetSmokeTests).Methods("GET")
 	smoketestRouter.HandleFunc("/smoketests/{id}", controllers.GetSmokeTestById).Methods("GET")
 	smoketestRouter.HandleFunc("/smoketests/users/{id}", controllers.GetSmokeTestsByUser).Methods("GET")
+	smoketest.HandleFunc("/smoketests/{id}", controllers.DeleteSmoketest).Methods("DELETE")
 	router.PathPrefix("/smoketests").Handler(negroni.New(
 		negroni.HandlerFunc(common.Authorize),
 		negroni.Wrap(smoketestRouter),
