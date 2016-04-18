8b4ad67b1e0c090c4e5ec97f7310618bd4b89959
alainglez <alain.gonzalez@gmail.com>
2016-04-18 16:34:25 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
-import
+
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/alainglez/go-smoke/common"
+	"github.com/alainglez/go-smoke/routers"
+)
+
+//Entry point of the program
+func main() {
+
+	// Calls startup logic
+	common.StartUp()
+	// Get the mux router object
+	router := routers.InitRoutes()
+	// Create a negroni instance
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	server := &http.Server{
+		Addr:    common.AppConfig.Server,
+		Handler: n,
+	}
+	log.Println("Listening...")
+	server.ListenAndServe()
+}
