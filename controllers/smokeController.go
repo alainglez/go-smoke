5e6f3ec6a7164f7fadda498f473726504d35fe95
alainglez <alain.gonzalez@gmail.com>
2016-04-19 17:00:15 -0400

    Update smokeController.go

--- controllers/smokeController.go
+++ controllers/smokeController.go
@@ -44,15 +44,7 @@ func RunSmokeTest(w http.ResponseWriter, r *http.Request) {
 	repourls := &data.UrlRepository{curls}
 	testurls := repourls.GetBySite(smoketest.SiteId.String())
 	// Run smoke test
-	if err := Smoke(smoketest,testurls); err != nil {
-		common.DisplayAppError(
-			w,
-			err,
-			"An unexpected error has occurred",
-			500,
-		)
-		return
-	}
+	Smoke(smoketest,testurls)
 	// Update the smoke test record with results
 	if err := repo.Update(smoketest); err != nil {
 		common.DisplayAppError(
